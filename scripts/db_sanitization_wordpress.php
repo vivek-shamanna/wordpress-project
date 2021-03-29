50d3ce2663f22bc656f5a64f89184fd364fa244f
vivek-shamanna <vivek.hs@pantheon.io>
2021-03-29 18:01:00 +0530

    test commit from VS

--- scripts/db_sanitization_wordpress.php
+++ scripts/db_sanitization_wordpress.php
@@ -9,3 +9,4 @@ if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
   // Query the database to set all user's email addresses to username@localhost.
   $wpdb->query("UPDATE wp_users SET user_email = CONCAT(user_login, '@localhost'), user_pass = MD5(CONCAT('MILDSECRET', user_login)), user_activation_key = '';");
 }
+
